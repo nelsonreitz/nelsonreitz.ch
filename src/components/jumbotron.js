b1559ab1e7cd6a5613cac3043ad402015f737f6f
Nelson Reitz <nelson.reitz@gmail.com>
2019-04-02 11:43:38 +0200

    Add the jumbotron component

---
+++ src/components/jumbotron.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Jumbotron = () => (
+  <header>
+    <h1>Nelson Reitz, Swiss Front-end Designer and Developer</h1>
+    <p>I am a Swiss Front-end Designer, I build user interfaces for websites and apps. Currently based in Lausanne working at the DevFactory agency.</p>
+  </header>
+)
+
+export default Jumbotron
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,14 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Jumbotron from "../components/jumbotron"
 import Section from "../components/section"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <Jumbotron />
+
     <Section title="My work">
       content
     </Section>
