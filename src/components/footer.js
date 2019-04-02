eb47b78774fb161838e574185f2143be19f46462
Nelson Reitz <nelson.reitz@gmail.com>
2019-04-02 11:19:44 +0200

    Add the footer component

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Footer = () => (
+  <footer>
+    <div className="container">
+      <h2 className="sectionTitle">Contact</h2>
+      <p>Feel free to contact me by email if you have questions or just want to discuss about front-end stuff. I am eager to share my passion for detailed, clean and user-centered products with you! You can also connect with me on <a href="https://ch.linkedin.com/in/nelsonreitz/en" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+      <a href="mailto:nelson.reitz@gmail.com">Get in touch</a>
+    </div>
+  </footer>
+)
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Footer from "../components/footer"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Footer />
   </Layout>
 )
 
