6eddf2a46f370fbc98c68f09a595159301ca18ca
Nelson Reitz <nailsonne@gmail.com>
2019-04-08 20:21:53 +0200

    Add jumbotron style

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -4,8 +4,9 @@ import styles from "./jumbotron.module.css"
 const Jumbotron = () => (
   <div className={styles.jumbotron}>
     <div className="container">
-      <h1>Nelson Reitz, Swiss Front-end Designer and Developer</h1>
-      <p>I am a Swiss Front-end Designer, I build user interfaces for websites and apps. Currently based in Lausanne working at the DevFactory agency.</p>
+      <h1>Hi! I am Nelson Reitz, a Swiss Front-end Designer and Developer <span className={styles.emoji} role="img" aria-label="Fire">☀️</span></h1>
+      <p>I build user interfaces for websites and apps. Currently based in Lausanne working at the DevFactory agency.</p>
+      <a href="mailto:nelson.reitz@gmail.com">Get in touch</a>
     </div>
   </div>
 )
