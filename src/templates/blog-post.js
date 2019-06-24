ddc8b5afbaf4bc07886989466a174eeb48d71860
Nelson Reitz <nailsonne@gmail.com>
2019-06-24 15:04:24 +0200

    Add page title to blog post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,12 +1,15 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 import styles from "../styles/blogPost.module.css"
 
 export default ({ data }) => {
   const post = data.markdownRemark
+  const pageTitle = post.frontmatter.title + ` | Nelson Reitz`
   return (
     <Layout>
+      <SEO title={pageTitle} />
       <div className="container">
         <div className={styles.blogPost}>
           <h1 className={styles.title}>{post.frontmatter.title}</h1>
