e59819879f63ebfbee85baa61343ae74272c34be
Nelson Reitz <nailsonne@gmail.com>
2019-07-19 17:12:24 +0200

    Move styles in jumbotron component

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,14 +1,73 @@
 import React from "react"
+import { css } from "@emotion/core"
+import styled from "@emotion/styled"
+import { colors, mediaQueries } from "../tokens"
 import Container from "./container"
-import styles from "../styles/jumbotron.module.css"
+
+const styles = css`
+  padding-top: 2rem;
+  padding-bottom: 4rem;
+  ${mediaQueries.sm} {
+    padding-top: 10vh;
+  }
+
+  p {
+    margin-bottom: 1.5rem;
+    font-size: 1.5rem;
+    line-height: 1.5;
+    hyphens: initial;
+    ${mediaQueries.sm} {
+      margin-bottom: 2rem;
+      font-size: 2rem;
+    }
+  }
+`
+
+const Title = styled.h1`
+  margin-bottom: 1.5rem;
+  font-size: 3rem;
+  ${mediaQueries.sm} {
+    font-size: 4rem;
+  }
+`
+
+const Accent = styled.p`
+  margin-bottom: 1.5rem;
+
+  em {
+    font-style: initial;
+    color: ${colors.red};
+  }
+
+  span {
+    vertical-align: middle;
+  }
+`
+
+const Cta = styled.a`
+  display: inline-block;
+  padding: 1.5rem 3.5rem;
+  font-size: 1.25rem;
+  font-weight: 600;
+  color: ${colors.white};
+  text-decoration: none;
+  border-radius: 4px;
+  background-color: ${colors.primary};
+  transition: background-color .1s ease-in-out;
+  &:hover {
+    background-color: ${colors.red};
+  }
+`
 
 const Jumbotron = () => (
-  <div className={styles.jumbotron}>
+  <div css={styles}>
     <Container>
-      <h1>Hi! I am Nelson Reitz, a Swiss Front-end Developer</h1>
+      <Title>Hi! I am Nelson Reitz, a Swiss Front-end Developer</Title>
       <p>I build user interfaces for websites and apps. Currently based in Lausanne. Previously @ DevFactory.</p>
-      <p className={styles.accent}><em>Now available for hire</em> <span className={styles.emoji} role="img" aria-label="Sparkles">✨</span></p>
-      <a className={styles.cta} href="mailto:nelson.reitz@gmail.com">Get in touch</a>
+      <Accent>
+        <em>Now available for hire</em> <span role="img" aria-label="Sparkles">✨</span>
+      </Accent>
+      <Cta href="mailto:nelson.reitz@gmail.com">Get in touch</Cta>
     </Container>
   </div>
 )
